482665d9172ed43e4fa27ab73caa077c9e8325d7
Tim0theus <beckmann.timm@web.de>
2017-12-22 05:37:46 +0100

    changed stuff for printing, added repetition, cold miss solved. ...

--- sheet2/stride.c
+++ sheet2/stride.c
@@ -6,9 +6,10 @@
 
 int debug = 0;
 double *results;
-size_t maxStride = 1024*500;
-size_t stepSize = 1024;
+size_t *strides;
+size_t maxStride = 1024;
 size_t clockRate = 400 * 1000000;
+int count = 0;
 
 static inline double gettime(void) {
     struct timespec ts = {};
@@ -19,33 +20,36 @@ static inline double gettime(void) {
     return (ts.tv_sec + ts.tv_nsec / 1.0e9);
 }
 
-void accessArray(uint8_t* arr, size_t length, size_t run, size_t stride) {
-	double start, stop;
-	size_t i = 0;
-	uint8_t sum = 0;
+void accessArray(uint8_t* arr, size_t length, size_t stride) {
+	double start, stop, sum;
+	size_t i, j;
 
-    start = gettime();
-	for (i = stride;i < length; i+=stride) {
-		sum += arr[i];
+	sum = 0;
+
+	for (j = 0; j < 6; j++) {
+		start = gettime();
+		for (i = 0;i < 100; i++) {
+			sum += arr[(i*stride)%length];
+		}
+		stop = gettime();
+		sum += stop - start;
 	}
-    stop = gettime();
 
-    results[run] = stop - start;
-    results[run] = results[run]*clockRate / (length/stride);
-
-    if (sum > 0)
-    	printf("%d",sum);
+	strides[count] = stride;
+    results[count] = sum / 6.0;
+    results[count] = results[count]*clockRate / 100.0;
+    count++;
 }
 
 static void printresults() {
     size_t i;
-    double tmp = 0;
+    //double tmp = 0;
 
-	for (i = 1; i*stepSize < maxStride; i++) {
-			if (tmp == results[i-1]) continue;
-			tmp = results[i-1];
+	for (i = 0; i < count; i++) {
+			//if (tmp > (results[i-1] - 100) && tmp < (results[i-1] + 100)) continue;
+			//tmp = results[i-1];
 			printf("Stride: \tAvg CPI:\n");
-			printf("%ld:\t%9.9f \n", i*stepSize, results[i-1]);
+			printf("%9ld:\t%9.9f \n", strides[i], results[i]);
 		}
 	printf("\n");
 }
@@ -67,9 +71,12 @@ int main(int argc, char *argv[]) {
 
     uint8_t* arr = (uint8_t*)malloc(sizeof(uint8_t)*n);
     results = (double *)malloc(sizeof(double[maxStride]));
+    strides = (size_t *)malloc(sizeof(size_t[maxStride]));
 
-    for (i = 1; i*stepSize < maxStride; i++) {
-		accessArray(arr, n, i-1, i*stepSize);
+    if (arr[0] != 0) printf("Dummy");
+    count = 0;
+    for (i = 2; i <= maxStride; i*=2) {
+		accessArray(arr, n, i);
     }
     printf("\n");
 
