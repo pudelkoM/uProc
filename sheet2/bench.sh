c60e6b457468ae19974f58a5328c60d098eee2e8
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-01-13 19:35:09 +0100

    Add page benchmark results

--- sheet2/bench.sh
+++ sheet2/bench.sh
@@ -9,7 +9,7 @@ done
 
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 runs_pages=$(echo "1024*1024*32" | bc)
-printf "# memory	ns/access	total_accesses\n" > benchmark_page.txt
+printf "# pages	ns/access	total_accesses\n" > benchmark_page.txt
 for (( i = 0; i <= 20; i++ )); do
 	taskset -c 1 ./cache_size --page -n $(echo "2^$i" | bc) --runs $runs_pages --quiet >> benchmark_page.txt
 	taskset -c 1 ./cache_size --page -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs_pages --quiet >> benchmark_page.txt
@@ -17,7 +17,7 @@ done
 
 echo always > /sys/kernel/mm/transparent_hugepage/enabled
 runs_pages=$(echo "1024*1024*32" | bc)
-printf "# memory	ns/access	total_accesses\n" > benchmark_hugepage.txt
+printf "# pages	ns/access	total_accesses\n" > benchmark_hugepage.txt
 for (( i = 0; i <= 20; i++ )); do
 	taskset -c 1 ./cache_size --page -n $(echo "2^$i" | bc) --runs $runs_pages --quiet >> benchmark_hugepage.txt
 	taskset -c 1 ./cache_size --page -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs_pages --quiet >> benchmark_hugepage.txt
