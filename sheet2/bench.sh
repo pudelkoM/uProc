82891d5a981d68744dea5ce076fc3e4d67fdae01
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-01-13 19:07:04 +0100

    Use mmap for allocation. Lock pages in RAM. Add hugepage benchmark.

--- sheet2/bench.sh
+++ sheet2/bench.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-runs=$(echo "1024*1024*128" | bc)
-printf "# memory	ns/access	total_accesses\n" > benchmark.txt
+runs_cache=$(echo "1024*1024*128" | bc)
+printf "# memory	ns/access	total_accesses\n" > benchmark_cache.txt
 for (( i = 3; i <= 22; i++ )); do
-	taskset -c 1 ./cache_size -n $(echo "2^$i" | bc) --runs $runs --quiet >> benchmark.txt
-	taskset -c 1 ./cache_size -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs --quiet >> benchmark.txt
+	taskset -c 1 ./cache_size -n $(echo "2^$i" | bc) --runs $runs_cache --quiet >> benchmark_cache.txt
+	taskset -c 1 ./cache_size -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs_cache --quiet >> benchmark_cache.txt
+done
+
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+runs_pages=$(echo "1024*1024*32" | bc)
+printf "# memory	ns/access	total_accesses\n" > benchmark_page.txt
+for (( i = 0; i <= 20; i++ )); do
+	taskset -c 1 ./cache_size --page -n $(echo "2^$i" | bc) --runs $runs_pages --quiet >> benchmark_page.txt
+	taskset -c 1 ./cache_size --page -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs_pages --quiet >> benchmark_page.txt
+done
+
+echo always > /sys/kernel/mm/transparent_hugepage/enabled
+runs_pages=$(echo "1024*1024*32" | bc)
+printf "# memory	ns/access	total_accesses\n" > benchmark_hugepage.txt
+for (( i = 0; i <= 20; i++ )); do
+	taskset -c 1 ./cache_size --page -n $(echo "2^$i" | bc) --runs $runs_pages --quiet >> benchmark_hugepage.txt
+	taskset -c 1 ./cache_size --page -n $(echo "2^$i + 2^($i-1)" | bc) --runs $runs_pages --quiet >> benchmark_hugepage.txt
 done
--- sheet2/size.c
+++ sheet2/size.c
@@ -1,14 +1,13 @@
 #define _GNU_SOURCE
 #include <assert.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <time.h>
-
-int debug = 0;
-const size_t clockRate = 400 * 1000000;
 
 static inline double gettime(void) {
     struct timespec ts = {};
@@ -54,8 +53,28 @@ union mem_block {
     };
 } __attribute__((aligned(64)));
 static_assert(sizeof(union mem_block) == 64, "fail");
+static_assert(offsetof(union mem_block, next) == 0, "fail");
+static_assert(offsetof(union mem_block, visited) == 8, "fail");
+
+union page_block {
+    uint8_t raw[4096];
+    struct {
+        void *next;
+        uint64_t visited;
+    };
+} __attribute__((aligned(4096)));
+static_assert(sizeof(union page_block) == 4096, "fail");
+static_assert(offsetof(union page_block, next) == 0, "fail");
+static_assert(offsetof(union page_block, visited) == 8, "fail");
 
 static void fill_memory(union mem_block *mem, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        mem[i].next = mem + ((i + 1009) % (len));
+        mem[i].visited = 0;
+    }
+}
+
+static void fill_memory_page(union page_block *mem, size_t len) {
     for (size_t i = 0; i < len; ++i) {
         mem[i].next = mem + ((i + 1009) % (len));
         mem[i].visited = 0;
@@ -95,11 +114,17 @@ static struct measurement walk_array(union mem_block *mem, const size_t n, const
     return (struct measurement){(end - start), accesses, n};
 }
 
-static void printresults(const struct measurement result, bool quiet) {
+static void printresults(const struct measurement result, bool quiet, bool page_test) {
     if (!quiet) {
-        printf("Blocks: %lu (%s)\tAvg ns/access: %9.9f\tAccesses: %lu\n", result.blocks,
-               format_human(result.blocks * 64), result.time * 1e9 / result.accesses,
-               result.accesses);
+        if (page_test) {
+            printf("Pages: %lu (%s)\tAvg ns/access: %9.9f\tAccesses: %lu\n", result.blocks,
+                   format_human(result.blocks * 4096), result.time * 1e9 / result.accesses,
+                   result.accesses);
+        } else {
+            printf("Blocks: %lu (%s)\tAvg ns/access: %9.9f\tAccesses: %lu\n", result.blocks,
+                   format_human(result.blocks * 64), result.time * 1e9 / result.accesses,
+                   result.accesses);
+        }
     } else {
         printf("%lu\t%9.9f\t%lu\n", result.blocks * 64, result.time * 1e9 / result.accesses,
                result.accesses);
@@ -111,26 +136,48 @@ int main(int argc, char *argv[]) {
     size_t n = 0;
     uint64_t accs = 0;
     bool quiet = false;
+    bool page_test = false;
     for (int arg = 1; arg < argc; arg++) {
-        if (0 == strcmp("-d", argv[arg])) {
-            debug = 1;
-        }
         if (0 == strcmp("-n", argv[arg])) {
             n = atoi(argv[arg + 1]);
         }
         if (0 == strcmp("--runs", argv[arg])) {
             accs = atoi(argv[arg + 1]);
         }
+        if (0 == strcmp("--page", argv[arg])) {
+            page_test = true;
+        }
         if (0 == strcmp("--quiet", argv[arg])) {
             quiet = true;
         }
     }
-    union mem_block *mem = malloc(n * sizeof(*mem));
-    fill_memory(mem, n);
+    size_t sz;
+    union mem_block *mem;
+    if (page_test) {
+        sz = n * sizeof(union page_block);
+    } else {
+        sz = n * sizeof(union mem_block);
+    }
+    void *temp = mmap(NULL, sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    if (temp == MAP_FAILED) {
+        perror("mmap");
+        abort();
+    }
+    if (mlock(temp, sz)) {
+        perror("mlock");
+        abort();
+    }
+    mem = temp;
+    if (page_test)
+        fill_memory_page((union page_block *)mem, n);
+    else
+        fill_memory(mem, n);
+
     struct measurement mes = walk_array(mem, n, accs);
     if (n > accs)
         check_visted(mem, n);
-    printresults(mes, quiet);
-    free(mem);
+    printresults(mes, quiet, page_test);
+    munlock(temp, sz);
+    munmap(temp, sz);
     return 0;
 }
